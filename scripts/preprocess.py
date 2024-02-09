046d6481df925633dd2641d84ff8d3ad510101b0
Mahikshit Kurapati <kurapatimahikshit@gmail.com>
2024-02-08 21:03:50 -0500

    preprocess

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -10,34 +10,34 @@ from utils import remove_lines
 
 #%%
 # Converting all the parquet files to CSV
-convert_parquet_to_csv("/Users/mahi/Documents/therapeutic-companion-bot/data/parquet_files")
+convert_parquet_to_csv("../data/parquet_files")
 # %%
 # Now that we have all the CSV files, we preprocess them according to their specifications
 
 # Concatenating p1 and p2 as they are similar
-concat_csv("/Users/mahi/Documents/therapeutic-companion-bot/data/parquet_files/csv/p1.csv","../data/parquet_files/csv/p2.csv", "../data/parquet_files/csv/p1_p2.csv")
+concat_csv("../data/parquet_files/csv/p1.csv","../data/parquet_files/csv/p2.csv", "../data/parquet_files/csv/p1_p2.csv")
 
 #%%
 # As p3 and p6 are similar, we will concatenate them
-drop_col("/Users/mahi/Documents/therapeutic-companion-bot/data/parquet_files/csv/p6.csv", "transcript_id")
-concat_csv("/Users/mahi/Documents/therapeutic-companion-bot/data/parquet_files/csv/p3.csv", "../data/parquet_files/csv/p6.csv", "../data/parquet_files/csv/p3_p6.csv")
+drop_col("../data/parquet_files/csv/p6.csv", "transcript_id")
+concat_csv("../data/parquet_files/csv/p3.csv", "../data/parquet_files/csv/p6.csv", "../data/parquet_files/csv/p3_p6.csv")
 
 #%%
 # No preprocessing required for p4 as of now
 
 #%%
 # Splitting p5 into question and response and removing the unwanted string
-split_column("text", "<ASSISTANT>:", "/Users/mahi/Documents/therapeutic-companion-bot/data/parquet_files/csv/p5.csv")
-remove_string("/Users/mahi/Documents/therapeutic-companion-bot/data/parquet_files/csv/p5.csv", "Question", "<HUMAN>: ")
+split_column("text", "<ASSISTANT>:", "../data/parquet_files/csv/p5.csv")
+remove_string("../data/parquet_files/csv/p5.csv", "Question", "<HUMAN>: ")
 
 # %%
 # removing the string from p7 and splitting it into question and response
-remove_string("/Users/mahi/Documents/therapeutic-companion-bot/data/parquet_files/csv/p7.csv", "text", "<s>[INST] ")
-remove_lines("/Users/mahi/Documents/therapeutic-companion-bot/data/parquet_files/csv/p7.csv","text", "[/INST] ")
-split_column("text", "\[\/INST\] ", "/Users/mahi/Documents/therapeutic-companion-bot/data/parquet_files/csv/p7.csv")
+remove_string("../data/parquet_files/csv/p7.csv", "text", "<s>[INST] ")
+remove_lines("../data/parquet_files/csv/p7.csv","text", "[/INST] ")
+split_column("text", "\[\/INST\] ", "../data/parquet_files/csv/p7.csv")
 # %%
 # Removing the unwanted strings from p8
-remove_string("/Users/mahi/Documents/therapeutic-companion-bot/data/parquet_files/csv/p8.csv", "instruction", "### Instruction: ")
-remove_string("/Users/mahi/Documents/therapeutic-companion-bot/data/parquet_files/csv/p8.csv", "instruction", " ### Response:")
+remove_string("../data/parquet_files/csv/p8.csv", "instruction", "### Instruction: ")
+remove_string("../data/parquet_files/csv/p8.csv", "instruction", " ### Response:")
 
  # %%
